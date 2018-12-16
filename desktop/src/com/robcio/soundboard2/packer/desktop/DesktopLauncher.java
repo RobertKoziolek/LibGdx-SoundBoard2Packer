bf0ae6610fc32d31f9b26f86fa7e277433075a03
Robert Koziolek <robertkoziolek4c@gmail.com>
2018-12-16 17:02:17 +0100

    Project setup

---
+++ desktop/src/com/robcio/soundboard2/packer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.robcio.soundboard2.packer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.robcio.soundboard2.packer.SoundBoard2Packer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SoundBoard2Packer(), config);
+	}
+}
